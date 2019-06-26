9d20420f66cf6af85b3ff2367799dd31a7552279
Michele <Michele@10.0.5.64>
2019-06-26 23:01:55 +0200

    MatchCoordinator test fatto.

---
+++ MM/src/test/java/it/unicam/cs/pa/mastermind2019/TestMatchCoordinator.java
@@ -0,0 +1,168 @@
+package it.unicam.cs.pa.mastermind2019;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+public class TestMatchCoordinator {
+
+	@Test
+	public void testCheckPC() throws IOException {
+		
+		PlayerFactory p = new PlayerFactory();
+		Player bot = p.getPlayer("bot");
+		Player chicco = p.getPlayer("Bot");
+		GameParameters settings = new GameParameters(6, false);
+		Campo cam = new Campo(settings);
+		MatchCoordinator arbitro = new MatchCoordinator(settings, cam, bot, chicco);
+		ArrayList<Integer> app = new ArrayList<Integer>();
+		app.add(1);
+		app.add(2);
+		app.add(5);
+		app.add(4);
+		app.add(3);
+		app.add(7);
+		
+		cam.setDecodeArray(app);
+		
+		ArrayList<Pioli> risultato = new ArrayList<Pioli>();
+		risultato.add(Pioli.PC);
+		risultato.add(Pioli.PC);
+		risultato.add(Pioli.PC);
+		risultato.add(Pioli.PC);
+		risultato.add(Pioli.PC);
+		risultato.add(Pioli.PC);
+		
+		assertEquals(risultato, arbitro.check(app));
+	}
+	
+	@Test
+	public void testCheckPE() throws IOException
+	{
+		PlayerFactory p = new PlayerFactory();
+		Player bot = p.getPlayer("bot");
+		Player chicco = p.getPlayer("Bot");
+		GameParameters settings = new GameParameters(4, false);
+		Campo cam = new Campo(settings);
+		MatchCoordinator arbitro = new MatchCoordinator(settings, cam, bot, chicco);
+		ArrayList<Integer> app = new ArrayList<Integer>();
+		app.add(1);
+		app.add(2);
+		app.add(3);
+		app.add(4);
+		
+		ArrayList<Integer> app2 = new ArrayList<Integer>();
+		app2.add(4);
+		app2.add(3);
+		app2.add(2);
+		app2.add(1);		
+		
+		cam.setDecodeArray(app);
+		
+		ArrayList<Pioli> risultato = new ArrayList<Pioli>();
+		risultato.add(Pioli.PE);
+		risultato.add(Pioli.PE);
+		risultato.add(Pioli.PE);
+		risultato.add(Pioli.PE);
+		
+		assertEquals(risultato, arbitro.check(app2));
+	}
+	
+	@Test
+	public void testCheckBoth() throws IOException
+	{
+		PlayerFactory p = new PlayerFactory();
+		Player bot = p.getPlayer("bot");
+		Player chicco = p.getPlayer("Bot");
+		GameParameters settings = new GameParameters(4, false);
+		Campo cam = new Campo(settings);
+		MatchCoordinator arbitro = new MatchCoordinator(settings, cam, bot, chicco);
+		ArrayList<Integer> app = new ArrayList<Integer>();
+		app.add(1);
+		app.add(2);
+		app.add(3);
+		app.add(4);
+		
+		ArrayList<Integer> app2 = new ArrayList<Integer>();
+		app2.add(1);
+		app2.add(3);
+		app2.add(2);
+		app2.add(4);		
+		
+		cam.setDecodeArray(app);
+		
+		ArrayList<Pioli> risultato = new ArrayList<Pioli>();
+		risultato.add(Pioli.PC);
+		risultato.add(Pioli.PC);
+		risultato.add(Pioli.PE);
+		risultato.add(Pioli.PE);
+		
+		assertEquals(risultato, arbitro.check(app2));
+		
+		
+	}@Test
+	public void testCheckNull() throws IOException
+	{
+		PlayerFactory p = new PlayerFactory();
+		Player bot = p.getPlayer("bot");
+		Player chicco = p.getPlayer("Bot");
+		GameParameters settings = new GameParameters(4, false);
+		Campo cam = new Campo(settings);
+		MatchCoordinator arbitro = new MatchCoordinator(settings, cam, bot, chicco);
+		ArrayList<Integer> app = new ArrayList<Integer>();
+		app.add(1);
+		app.add(2);
+		app.add(3);
+		app.add(4);
+		
+		ArrayList<Integer> app2 = new ArrayList<Integer>();
+		app2.add(1);
+		app2.add(3);
+		app2.add(5);
+		app2.add(6);		
+		
+		cam.setDecodeArray(app);
+		
+		ArrayList<Pioli> risultato = new ArrayList<Pioli>();
+		risultato.add(Pioli.PC);
+		risultato.add(Pioli.PE);
+		
+		assertEquals(risultato, arbitro.check(app2));
+	}
+	
+	@Test
+	public void testIsWinnerTrue() throws IOException
+	{
+		
+		GameParameters settings = new GameParameters(4, false);
+		Campo cam = new Campo(settings);
+		
+		ArrayList<Pioli> risultato = new ArrayList<Pioli>();
+		risultato.add(Pioli.PC);
+		risultato.add(Pioli.PC);
+		risultato.add(Pioli.PC);
+		risultato.add(Pioli.PC);
+		
+		assertTrue(MatchCoordinator.isWinner(risultato, cam));
+	}
+
+	@Test
+	public void testIsWinnerFalse() throws IOException
+	{
+		
+		GameParameters settings = new GameParameters(4, false);
+		Campo cam = new Campo(settings);
+		
+		ArrayList<Pioli> risultato = new ArrayList<Pioli>();
+		risultato.add(Pioli.PC);
+		risultato.add(Pioli.PC);
+		risultato.add(Pioli.PC);
+		risultato.add(Pioli.PE);
+		
+		assertFalse(MatchCoordinator.isWinner(risultato, cam));
+	}
+
+}
